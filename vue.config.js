504785e46156accb355a37a131e8928381521c36
松井英俊 <partyrockeverydayimshufflin@gmail.com>
2020-04-09 00:26:41 +0900

    pwa設定追加

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  pwa: {
+    name: 'Mochikaeru',
+    themeColor: '#eb3f25',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  },
   "transpileDependencies": [
     "vuetify"
   ]
